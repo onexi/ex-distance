742bff1835b41dc938ca17d4671e4b0adc221227
qinziluo <qinziluo@mit.edu>
2017-03-20 10:14:07 -0400

    qinziluo changed it.

--- starter_code/exercise.js
+++ starter_code/exercise.js
@@ -1,27 +1,39 @@
 var exercise = {};
 
 // calculate distance between points
-exercise.distance = function(p1,p2){
+exercise.distance = function (p1, p2) {
     //-------------------
     //---- Your Code ----
     //-------------------
-    return 'Error: 1st function not implemented';
+    return Math.sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y));
+    //return 'Error: 1st function not implemented';
 };
 
 // calculate distance between new point and existing
-exercise.distances = function(newPoint,data){
+exercise.distances = function (newPoint, data) {
     //-------------------
     //---- Your Code ----
     //-------------------
-    return 'Error: 2nd function not implemented';
+    data.forEach(function (element) {
+        element.distance = Math.sqrt(Math.pow(element.x - newPoint.x, 2) + Math.pow(element.y - newPoint.y, 2));
+    })
+    return data;
+    //return 'Error: 2nd function not implemented';
 };
 
 // find k closest points
-exercise.findClosest = function(k,data){
+exercise.findClosest = function (k, data) {
     //-------------------
     //---- Your Code ----
     //-------------------
-    return 'Error: 3rd function not implemented';
+
+    data.sort(function(a,b){
+        return a.distance -b.distance;
+    });
+    
+    return data.slice(0,k);
+
+    //return 'Error: 3rd function not implemented';
 };
 
 module.exports = exercise;
